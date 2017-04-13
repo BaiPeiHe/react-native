ac89b49837c067b6dde9497a899a7a470404871f
BaiHe <392275709@qq.com>
2017-04-13 18:14:11 +0800

    0.1.1 底部导航菜单

--- rn_demo/index.ios.js
+++ rn_demo/index.ios.js
@@ -8,46 +8,92 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  Image,
   Text,
   View
 } from 'react-native';
 
+import TabNavigator from 'react-native-tab-navigator';
+
 export default class rn_demo extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+
+    constructor(props){
+        super(props);
+        this.state={
+            selectedTab: 'tb_popular',
+        }
+    }
+
+
+    render() {
+      return (
+        <View style={styles.container}>
+          <TabNavigator>
+              <TabNavigator.Item
+                  selected={this.state.selectedTab === 'tb_popular'}
+                  selectedTitleStyle={{color:'red'}}
+                  title="最热"
+                  renderIcon={() => <Image style={styles.iconImage} source={require('./res/images/ic_polular.png')} />}
+                  renderSelectedIcon={() => <Image style={[styles.iconImage,{tintColor:'red'}]} source={require('./res/images/ic_polular.png')} />}
+                  badgeText="1"
+                  onPress={() => this.setState({ selectedTab: 'tb_popular' })}>
+                  <View style={styles.page1}></View>
+              </TabNavigator.Item>
+              <TabNavigator.Item
+                  selected={this.state.selectedTab === 'tb_trending'}
+                  selectedTitleStyle={{color:'red'}}
+                  title="趋势"
+                  renderIcon={() => <Image style={styles.iconImage} source={require('./res/images/ic_trending.png')} />}
+                  renderSelectedIcon={() => <Image style={[styles.iconImage,{tintColor:'red'}]} source={require('./res/images/ic_trending.png')} />}
+                  onPress={() => this.setState({ selectedTab: 'tb_trending' })}>
+                  <View style={styles.page2}></View>
+              </TabNavigator.Item>
+              <TabNavigator.Item
+                  selected={this.state.selectedTab === 'tb_favorite'}
+                  selectedTitleStyle={{color:'red'}}
+                  title="收藏"
+                  renderIcon={() => <Image style={styles.iconImage} source={require('./res/images/ic_polular.png')} />}
+                  renderSelectedIcon={() => <Image style={[styles.iconImage,{tintColor:'red'}]} source={require('./res/images/ic_polular.png')} />}
+                  badgeText="1"
+                  onPress={() => this.setState({ selectedTab: 'tb_favorite' })}>
+                  <View style={styles.page1}></View>
+              </TabNavigator.Item>
+              <TabNavigator.Item
+                  selected={this.state.selectedTab === 'tb_my'}
+                  selectedTitleStyle={{color:'red'}}
+                  title="我的"
+                  renderIcon={() => <Image style={styles.iconImage} source={require('./res/images/ic_trending.png')} />}
+                  renderSelectedIcon={() => <Image style={[styles.iconImage,{tintColor:'red'}]} source={require('./res/images/ic_trending.png')} />}
+                  onPress={() => this.setState({ selectedTab: 'tb_my' })}>
+                  <View style={styles.page2}></View>
+              </TabNavigator.Item>
+          </TabNavigator>
+        </View>
+      );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container:{
+        flex:1,
+        backgroundColor: '#F5FCFF',
+    },
+
+    page1:{
+        flex:1,
+        backgroundColor:'red',
+    },
+
+    page2:{
+        flex:1,
+        backgroundColor:'yellow',
+    },
+
+    iconImage:{
+        height:22,
+        width:22
+    }
+
 });
 
 AppRegistry.registerComponent('rn_demo', () => rn_demo);
